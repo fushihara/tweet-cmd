75692d26b8f4c711663650d42d5bc766fe373bf2
Fushihara <fushihara@users.noreply.github.com>
2015-08-08 10:02:52 +0900

    初回コミット

---
+++ sleep/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sleep {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length == 0) {
+                return;
+            }
+            DateTime now=DateTime.Now;
+            DateTime sleepTime = getTime(args[0]);
+            if (sleepTime == null) {
+                return;
+            }
+            Console.WriteLine("wait for " + sleepTime.ToLocalTime());
+            Thread.Sleep(sleepTime - now);
+        }
+        private static DateTime getTime(String val) {
+            {
+                var regex1 = new Regex(@"(\d+)/(\d+)/(\d+) (\d+):(\d+):(\d+)");
+                var match = regex1.Match(val);
+                if (match.Success) {
+                    int year, month, date, hour, minute, second;
+                    year = int.Parse(match.Groups[1].Value);
+                    month = int.Parse(match.Groups[2].Value);
+                    date = int.Parse(match.Groups[3].Value);
+                    hour = int.Parse(match.Groups[4].Value);
+                    minute = int.Parse(match.Groups[5].Value);
+                    second = int.Parse(match.Groups[6].Value);
+                    return new DateTime(year, month, date, hour, minute, second);
+                }
+            }
+            {
+                var regex = new Regex(@"(\d+):(\d+):(\d+)");
+                var match = regex.Match(val);
+                if (match.Success) {
+                    int hour, minute, second;
+                    hour = int.Parse(match.Groups[1].Value);
+                    minute = int.Parse(match.Groups[2].Value);
+                    second = int.Parse(match.Groups[3].Value);
+                    if (24 <= hour) {
+                        hour -= 24;
+                    }
+                    var now = DateTime.Now;
+                    var set = new DateTime(now.Year, now.Month, now.Day, hour, minute, second);
+                    if (set < now) {
+                        //もし、setが過去だったら
+                        set += new TimeSpan(24, 0, 0);
+                        return set;
+                    } else {
+                        return set;
+                    }
+                }
+            }
+            {
+                //"1d1h1m1s"
+                var now = DateTime.Now;
+                var hasMatch = false;
+                var reg1 = new Regex(@"(\d+)d");
+                var reg2 = new Regex(@"(\d+)h");
+                var reg3 = new Regex(@"(\d+)m");
+                var reg4 = new Regex(@"(\d+)s");
+                var match1 = reg1.Match(val);
+                var match2 = reg2.Match(val);
+                var match3 = reg3.Match(val);
+                var match4 = reg4.Match(val);
+                if (match1.Success) {
+                    now += new TimeSpan(int.Parse(match1.Groups[1].Value), 0, 0, 0);
+                    hasMatch = true;
+                }
+                if (match2.Success) {
+                    now += new TimeSpan(int.Parse(match2.Groups[1].Value), 0, 0);
+                    hasMatch = true;
+                }
+                if (match3.Success) {
+                    now += new TimeSpan(0, int.Parse(match3.Groups[1].Value), 0);
+                    hasMatch = true;
+                }
+                if (match4.Success) {
+                    now += new TimeSpan(0, 0, int.Parse(match4.Groups[1].Value));
+                    hasMatch = true;
+                }
+                if (hasMatch) {
+                    return now;
+                }
+            }
+            return DateTime.Now;
+        }
+    }
+}
---
+++ twitter/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Core.Credentials;
+using Tweetinvi.Core.Interfaces.DTO;
+using Tweetinvi.Core.Parameters;
+
+namespace twitter {
+    class Program {
+        private String AccessToken = "";
+        private String AccessTokenSecret = "";
+        private String ConsumerKey = "";
+        private String ConsumerSecret = "";
+        private String Message;
+        private List<String> Images = new List<string>();
+        static void Main(string[] args) {
+            Program pg = new Program();
+            pg.readCommandLineArgs(args);
+            pg.doPost();
+        }
+        public void readCommandLineArgs(string[] args) {
+            for (int i = 0; i < args.Length; i++) {
+                String val = args[i];
+                Boolean hasNext = (i + 1) < args.Length;
+                String nextVal = hasNext ? args[i + 1] : "";
+                if (!hasNext) {
+                    return;
+                } else if (val == "-at") {
+                    AccessToken = nextVal;
+                    i++;
+                } else if (val == "-ats") {
+                    AccessTokenSecret = nextVal;
+                    i++;
+                } else if (val == "-ck") {
+                    ConsumerKey = nextVal;
+                    i++;
+                } else if (val == "-cs") {
+                    ConsumerSecret = nextVal;
+                    i++;
+                } else if (val == "-message") {
+                    Message = nextVal;
+                    i++;
+                } else if (val == "-image") {
+                    Images.Add(nextVal);
+                    i++;
+                }
+            }
+            if (AccessToken == "") {
+                throw new ArgumentException("AccessToken is empty.");
+            }
+            if (AccessTokenSecret == "") {
+                throw new ArgumentException("AccessTokenSecret is empty.");
+            }
+            if (ConsumerKey == "") {
+                throw new ArgumentException("ConsumerKey is empty.");
+            }
+            if (ConsumerSecret == "") {
+                throw new ArgumentException("ConsumerSecret is empty.");
+            }
+            if (Message == "") {
+                throw new ArgumentException("Message is empty.");
+            }
+        }
+        public void doPost() {
+            var creds = new TwitterCredentials(ConsumerKey, ConsumerSecret, AccessToken, AccessTokenSecret);
+            var tweet = Auth.ExecuteOperationWithCredentials(creds, () => {
+                List<IMedia> medias = new List<IMedia>();
+                foreach (String path in Images) {
+                    var image = File.ReadAllBytes(path);
+                    var media = Upload.UploadImage(image);
+                    medias.Add(media);
+                }
+                if (medias.Count == 0) {
+                    //テキストのみ
+                    return Tweet.PublishTweet(Message);
+                } else {
+                    //画像付き
+                    return Tweet.PublishTweet(Message, new PublishTweetOptionalParameters {
+                        Medias = medias
+                    });
+                }
+            });
+            Console.WriteLine("https://twitter.com/" + tweet.CreatedBy.ScreenName + "/status/" + tweet.IdStr);
+        }
+    }
+}
